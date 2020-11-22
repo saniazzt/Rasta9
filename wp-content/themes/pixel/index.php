d66a3f35ddba434ae228f90bf782b19ee117f52a
saniazzt13 <saniaezzati1384@gmail.com>
2020-11-22 17:20:37 +0330

    first commit

---
+++ wp-content/themes/pixel/index.php
@@ -0,0 +1,18 @@
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  
+         
+ 
+ 
+ 
+ 
+ 
+<h2><?php the_title() ?></h2>
+ 
+ 
+ 
+ 
+ 
+ 
+        <?php the_content() ?>
+     
+    <?php endwhile; else : echo ' There are no posts! '; endif; ?>
